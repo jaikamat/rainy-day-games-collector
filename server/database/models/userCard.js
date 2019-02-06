5d71cd2cc55d92c04b28f2a9f56687726649d0c4
Jai Kamat <jaipkamat@gmail.com>
2019-02-06 13:06:51 -0800

    User deletion cascades into userCard (wishlist) table

--- server/database/models/userCard.js
+++ server/database/models/userCard.js
@@ -25,7 +25,7 @@ module.exports = (sequelize, Sequelize) => {
 
     // Create associations
     UserCard.associate = (models) => {
-        UserCard.belongsTo(models.user, { foreignKey: 'user_id' });
+        UserCard.belongsTo(models.user, { foreignKey: 'user_id', onDelete: 'cascade' });
         UserCard.belongsTo(models.card, { foreignKey: 'card_id' });
     };
     
