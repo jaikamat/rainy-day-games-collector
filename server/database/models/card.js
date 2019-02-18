8ec8c77dc76f30b64a81ccbb34babd8da7403f48
Jai Kamat <jaipkamat@gmail.com>
2019-02-18 15:10:39 -0800

    Implemented major change to Card model and seeded data

--- server/database/models/card.js
+++ server/database/models/card.js
@@ -5,26 +5,56 @@ module.exports = (sequelize, Sequelize) => {
             primaryKey: true,
             type: Sequelize.INTEGER
         },
-        isFlip: { type: Sequelize.BOOLEAN },
-        color: { type: Sequelize.STRING },
-        rarity: { type: Sequelize.STRING },
-        title: { type: Sequelize.STRING },
-        setCode: { type: Sequelize.STRING },
-        price: { type: Sequelize.DECIMAL },
-        quantity: { type: Sequelize.INTEGER }
+        scryfall_id: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        title: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        scryfall_uri: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        image_uri: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        colors: {
+            type: Sequelize.STRING, // Note: joined from source array into single string
+            allowNull: false
+        },
+        reserved: {
+            type: Sequelize.BOOLEAN,
+            allowNull: false
+        },
+        setCode: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        rarity: {
+            type: Sequelize.STRING,
+            allowNull: false
+        }
     }, {
         hooks: {
-            afterSave: (card) => {
-                for (key in card) {
-                    if (key === 'attributes') {
-                        console.log(card[key]);
+            // After a card entry is created, create
+            // a cardInventory 1:1 association entry to record price and quantity later
+            afterCreate: (card) => {
+                sequelize.models.card.findOne({
+                    where: {
+                        scryfall_id: card.dataValues.scryfall_id
                     }
-                }
-                // sequelize.models.cardInventory.create({
-                //     card_id: card.card_id,
-                //     price: 0,
-                //     quantity: 0
-                // })
+                }).then(card => {
+                    return sequelize.models.cardInventory.create({
+                        card_id: card.card_id,
+                        price: 0,
+                        quantity: 0
+                    })
+                }).catch(error => {
+                    console.log(error);
+                });
             }
         }
     });
--- server/database/seed.js
+++ server/database/seed.js
@@ -10,277 +10,277 @@ const cardData = [
        "setCode":"RNA",
        "price":0.25,
        "quantity":20
+    },
+    {
+       "id":2,
+       "isFlip":false,
+       "color":"A",
+       "rarity":"U",
+       "title":"Dovins Automaton",
+       "setCode":"RNA",
+       "price":1,
+       "quantity":3
+    },
+    {
+       "id":3,
+       "isFlip":false,
+       "color":"A",
+       "rarity":"U",
+       "title":"Gate Colossus",
+       "setCode":"RNA",
+       "price":0.5,
+       "quantity":8
+    },
+    {
+       "id":4,
+       "isFlip":false,
+       "color":"A",
+       "rarity":"R",
+       "title":"Glass of the Guildpact",
+       "setCode":"RNA",
+       "price":2,
+       "quantity":4
+    },
+    {
+       "id":5,
+       "isFlip":false,
+       "color":"A",
+       "rarity":"C",
+       "title":"Gruul Locket",
+       "setCode":"RNA",
+       "price":0.25,
+       "quantity":20
+    },
+    {
+       "id":6,
+       "isFlip":false,
+       "color":"A",
+       "rarity":"U",
+       "title":"Junktroller",
+       "setCode":"RNA",
+       "price":0.5,
+       "quantity":14
+    },
+    {
+       "id":7,
+       "isFlip":false,
+       "color":"A",
+       "rarity":"C",
+       "title":"Orzhov Locket",
+       "setCode":"RNA",
+       "price":0.25,
+       "quantity":20
+    },
+    {
+       "id":8,
+       "isFlip":false,
+       "color":"A",
+       "rarity":"C",
+       "title":"Rakdos Locket",
+       "setCode":"RNA",
+       "price":0.25,
+       "quantity":20
+    },
+    {
+       "id":9,
+       "isFlip":false,
+       "color":"A",
+       "rarity":"U",
+       "title":"Scrabbling Claws",
+       "setCode":"RNA",
+       "price":0.5,
+       "quantity":10
+    },
+    {
+       "id":10,
+       "isFlip":false,
+       "color":"A",
+       "rarity":"U",
+       "title":"Screaming Shield",
+       "setCode":"RNA",
+       "price":0.5,
+       "quantity":11
+    },
+    {
+       "id":11,
+       "isFlip":false,
+       "color":"A",
+       "rarity":"C",
+       "title":"Simic Locket",
+       "setCode":"RNA",
+       "price":0.25,
+       "quantity":20
+    },
+    {
+       "id":12,
+       "isFlip":false,
+       "color":"A",
+       "rarity":"U",
+       "title":"Sphinx of the Guildpact",
+       "setCode":"RNA",
+       "price":0.5,
+       "quantity":9
+    },
+    {
+       "id":13,
+       "isFlip":false,
+       "color":"A",
+       "rarity":"R",
+       "title":"Tome of the Guildpact",
+       "setCode":"RNA",
+       "price":2,
+       "quantity":4
+    },
+    {
+       "id":14,
+       "isFlip":false,
+       "color":"B",
+       "rarity":"R",
+       "title":"Awaken the Erstwhile",
+       "setCode":"RNA",
+       "price":2,
+       "quantity":4
+    },
+    {
+       "id":15,
+       "isFlip":false,
+       "color":"B",
+       "rarity":"U",
+       "title":"Bankrupt in Blood",
+       "setCode":"RNA",
+       "price":0.5,
+       "quantity":8
+    },
+    {
+       "id":16,
+       "isFlip":false,
+       "color":"B",
+       "rarity":"C",
+       "title":"Blade Juggler",
+       "setCode":"RNA",
+       "price":0.25,
+       "quantity":18
+    },
+    {
+       "id":17,
+       "isFlip":false,
+       "color":"B",
+       "rarity":"C",
+       "title":"Bladebrand",
+       "setCode":"RNA",
+       "price":0.25,
+       "quantity":19
+    },
+    {
+       "id":18,
+       "isFlip":false,
+       "color":"B",
+       "rarity":"U",
+       "title":"Bloodmist Infiltrator",
+       "setCode":"RNA",
+       "price":0.5,
+       "quantity":6
+    },
+    {
+       "id":19,
+       "isFlip":false,
+       "color":"B",
+       "rarity":"C",
+       "title":"Carrion Imp",
+       "setCode":"RNA",
+       "price":0.25,
+       "quantity":20
+    },
+    {
+       "id":20,
+       "isFlip":false,
+       "color":"B",
+       "rarity":"C",
+       "title":"Catacomb Crocodile",
+       "setCode":"RNA",
+       "price":0.25,
+       "quantity":20
+    },
+    {
+       "id":21,
+       "isFlip":false,
+       "color":"B",
+       "rarity":"U",
+       "title":"Clear the Stage",
+       "setCode":"RNA",
+       "price":0.5,
+       "quantity":8
+    },
+    {
+       "id":22,
+       "isFlip":false,
+       "color":"B",
+       "rarity":"C",
+       "title":"Consign to the Pit",
+       "setCode":"RNA",
+       "price":0.25,
+       "quantity":19
+    },
+    {
+       "id":23,
+       "isFlip":false,
+       "color":"B",
+       "rarity":"U",
+       "title":"Cry of the Carnarium",
+       "setCode":"RNA",
+       "price":0.5,
+       "quantity":3
+    },
+    {
+       "id":24,
+       "isFlip":false,
+       "color":"B",
+       "rarity":"C",
+       "title":"Dead Revels",
+       "setCode":"RNA",
+       "price":0.25,
+       "quantity":20
+    },
+    {
+       "id":25,
+       "isFlip":false,
+       "color":"B",
+       "rarity":"C",
+       "title":"Debtors Transport",
+       "setCode":"RNA",
+       "price":0.25,
+       "quantity":20
+    },
+    {
+       "id":26,
+       "isFlip":false,
+       "color":"B",
+       "rarity":"U",
+       "title":"Drill Bit",
+       "setCode":"RNA",
+       "price":0.5,
+       "quantity":10
+    },
+    {
+       "id":27,
+       "isFlip":false,
+       "color":"B",
+       "rarity":"R",
+       "title":"Font of Agonies",
+       "setCode":"RNA",
+       "price":2,
+       "quantity":4
+    },
+    {
+       "id":28,
+       "isFlip":false,
+       "color":"B",
+       "rarity":"C",
+       "title":"Grotesque Demise",
+       "setCode":"RNA",
+       "price":0.25,
+       "quantity":20
     }
-   //  {
-   //     "id":2,
-   //     "isFlip":false,
-   //     "color":"A",
-   //     "rarity":"U",
-   //     "title":"Dovins Automaton",
-   //     "setCode":"RNA",
-   //     "price":1,
-   //     "quantity":3
-   //  },
-   //  {
-   //     "id":3,
-   //     "isFlip":false,
-   //     "color":"A",
-   //     "rarity":"U",
-   //     "title":"Gate Colossus",
-   //     "setCode":"RNA",
-   //     "price":0.5,
-   //     "quantity":8
-   //  },
-   //  {
-   //     "id":4,
-   //     "isFlip":false,
-   //     "color":"A",
-   //     "rarity":"R",
-   //     "title":"Glass of the Guildpact",
-   //     "setCode":"RNA",
-   //     "price":2,
-   //     "quantity":4
-   //  },
-   //  {
-   //     "id":5,
-   //     "isFlip":false,
-   //     "color":"A",
-   //     "rarity":"C",
-   //     "title":"Gruul Locket",
-   //     "setCode":"RNA",
-   //     "price":0.25,
-   //     "quantity":20
-   //  },
-   //  {
-   //     "id":6,
-   //     "isFlip":false,
-   //     "color":"A",
-   //     "rarity":"U",
-   //     "title":"Junktroller",
-   //     "setCode":"RNA",
-   //     "price":0.5,
-   //     "quantity":14
-   //  },
-   //  {
-   //     "id":7,
-   //     "isFlip":false,
-   //     "color":"A",
-   //     "rarity":"C",
-   //     "title":"Orzhov Locket",
-   //     "setCode":"RNA",
-   //     "price":0.25,
-   //     "quantity":20
-   //  },
-   //  {
-   //     "id":8,
-   //     "isFlip":false,
-   //     "color":"A",
-   //     "rarity":"C",
-   //     "title":"Rakdos Locket",
-   //     "setCode":"RNA",
-   //     "price":0.25,
-   //     "quantity":20
-   //  },
-   //  {
-   //     "id":9,
-   //     "isFlip":false,
-   //     "color":"A",
-   //     "rarity":"U",
-   //     "title":"Scrabbling Claws",
-   //     "setCode":"RNA",
-   //     "price":0.5,
-   //     "quantity":10
-   //  },
-   //  {
-   //     "id":10,
-   //     "isFlip":false,
-   //     "color":"A",
-   //     "rarity":"U",
-   //     "title":"Screaming Shield",
-   //     "setCode":"RNA",
-   //     "price":0.5,
-   //     "quantity":11
-   //  },
-   //  {
-   //     "id":11,
-   //     "isFlip":false,
-   //     "color":"A",
-   //     "rarity":"C",
-   //     "title":"Simic Locket",
-   //     "setCode":"RNA",
-   //     "price":0.25,
-   //     "quantity":20
-   //  },
-   //  {
-   //     "id":12,
-   //     "isFlip":false,
-   //     "color":"A",
-   //     "rarity":"U",
-   //     "title":"Sphinx of the Guildpact",
-   //     "setCode":"RNA",
-   //     "price":0.5,
-   //     "quantity":9
-   //  },
-   //  {
-   //     "id":13,
-   //     "isFlip":false,
-   //     "color":"A",
-   //     "rarity":"R",
-   //     "title":"Tome of the Guildpact",
-   //     "setCode":"RNA",
-   //     "price":2,
-   //     "quantity":4
-   //  },
-   //  {
-   //     "id":14,
-   //     "isFlip":false,
-   //     "color":"B",
-   //     "rarity":"R",
-   //     "title":"Awaken the Erstwhile",
-   //     "setCode":"RNA",
-   //     "price":2,
-   //     "quantity":4
-   //  },
-   //  {
-   //     "id":15,
-   //     "isFlip":false,
-   //     "color":"B",
-   //     "rarity":"U",
-   //     "title":"Bankrupt in Blood",
-   //     "setCode":"RNA",
-   //     "price":0.5,
-   //     "quantity":8
-   //  },
-   //  {
-   //     "id":16,
-   //     "isFlip":false,
-   //     "color":"B",
-   //     "rarity":"C",
-   //     "title":"Blade Juggler",
-   //     "setCode":"RNA",
-   //     "price":0.25,
-   //     "quantity":18
-   //  },
-   //  {
-   //     "id":17,
-   //     "isFlip":false,
-   //     "color":"B",
-   //     "rarity":"C",
-   //     "title":"Bladebrand",
-   //     "setCode":"RNA",
-   //     "price":0.25,
-   //     "quantity":19
-   //  },
-   //  {
-   //     "id":18,
-   //     "isFlip":false,
-   //     "color":"B",
-   //     "rarity":"U",
-   //     "title":"Bloodmist Infiltrator",
-   //     "setCode":"RNA",
-   //     "price":0.5,
-   //     "quantity":6
-   //  },
-   //  {
-   //     "id":19,
-   //     "isFlip":false,
-   //     "color":"B",
-   //     "rarity":"C",
-   //     "title":"Carrion Imp",
-   //     "setCode":"RNA",
-   //     "price":0.25,
-   //     "quantity":20
-   //  },
-   //  {
-   //     "id":20,
-   //     "isFlip":false,
-   //     "color":"B",
-   //     "rarity":"C",
-   //     "title":"Catacomb Crocodile",
-   //     "setCode":"RNA",
-   //     "price":0.25,
-   //     "quantity":20
-   //  },
-   //  {
-   //     "id":21,
-   //     "isFlip":false,
-   //     "color":"B",
-   //     "rarity":"U",
-   //     "title":"Clear the Stage",
-   //     "setCode":"RNA",
-   //     "price":0.5,
-   //     "quantity":8
-   //  },
-   //  {
-   //     "id":22,
-   //     "isFlip":false,
-   //     "color":"B",
-   //     "rarity":"C",
-   //     "title":"Consign to the Pit",
-   //     "setCode":"RNA",
-   //     "price":0.25,
-   //     "quantity":19
-   //  },
-   //  {
-   //     "id":23,
-   //     "isFlip":false,
-   //     "color":"B",
-   //     "rarity":"U",
-   //     "title":"Cry of the Carnarium",
-   //     "setCode":"RNA",
-   //     "price":0.5,
-   //     "quantity":3
-   //  },
-   //  {
-   //     "id":24,
-   //     "isFlip":false,
-   //     "color":"B",
-   //     "rarity":"C",
-   //     "title":"Dead Revels",
-   //     "setCode":"RNA",
-   //     "price":0.25,
-   //     "quantity":20
-   //  },
-   //  {
-   //     "id":25,
-   //     "isFlip":false,
-   //     "color":"B",
-   //     "rarity":"C",
-   //     "title":"Debtors Transport",
-   //     "setCode":"RNA",
-   //     "price":0.25,
-   //     "quantity":20
-   //  },
-   //  {
-   //     "id":26,
-   //     "isFlip":false,
-   //     "color":"B",
-   //     "rarity":"U",
-   //     "title":"Drill Bit",
-   //     "setCode":"RNA",
-   //     "price":0.5,
-   //     "quantity":10
-   //  },
-   //  {
-   //     "id":27,
-   //     "isFlip":false,
-   //     "color":"B",
-   //     "rarity":"R",
-   //     "title":"Font of Agonies",
-   //     "setCode":"RNA",
-   //     "price":2,
-   //     "quantity":4
-   //  },
-   //  {
-   //     "id":28,
-   //     "isFlip":false,
-   //     "color":"B",
-   //     "rarity":"C",
-   //     "title":"Grotesque Demise",
-   //     "setCode":"RNA",
-   //     "price":0.25,
-   //     "quantity":20
-   //  }
 ];
 
 const scryfallData = [
@@ -337,6 +337,19 @@ const userCardData = [{
    card_id: 12
 }];
 
+const manipScryfallData = (card) => {
+   return {
+      scryfall_id: card.id,
+      title: card.name,
+      scryfall_uri: card.scryfall_uri,
+      image_uri: card.image_uris.normal,
+      colors: card.colors.join(''),
+      reserved: card.reserved,
+      setCode: card.set,
+      rarity: card.rarity
+   }
+}
+
 // Seeds the database with test data
 const seedTest = () => {
    return models.sequelize.sync({ force: true })
@@ -345,9 +358,12 @@ const seedTest = () => {
    }).then(() => {
       return Promise.all(userData.map(data => models.user.create(data))); // Seed user data
    }).then(() => {
-      return Promise.all(cardData.map((data) => models.card.create(data))); // Seed card data
+      return Promise.all(scryfallData.map((data) => {
+         let manipulatedData = manipScryfallData(data);
+         models.card.create(manipulatedData); // Seed card data
+      }));
    }).then(() => {
-      // return Promise.all(userCardData.map(data => models.userCard.create(data))); // Seed wishlist data
+      return Promise.all(userCardData.map(data => models.userCard.create(data))); // Seed wishlist data
    }).catch((error) => {
       console.log(error);
    });
